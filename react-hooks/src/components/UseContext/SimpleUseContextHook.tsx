f8ca1da60d4903b5ccf3462126bd1da3bf65c881
HVMS <viralsid2330@gmail.com>
2024-05-24 16:17:38 -0300

    chore: Add theme toggle functionality to SimpleUseContextHook component

--- react-hooks/src/components/UseContext/SimpleUseContextHook.tsx
+++ react-hooks/src/components/UseContext/SimpleUseContextHook.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../Theme/useThemeContext";
+import { ThemeProvider, ThemeContext } from "@emotion/react";
 
 const SimpleUseContextHook = () => {
-    
+
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    }
+
     return (
-        <div>
-            <h1>Simple UseContext hook</h1>
-        </div>
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            <div>
+                <Button onClick={toggleTheme}>Toggle Theme</Button>
+            </div>
+        </ThemeContext.Provider>
     )
 }
 
