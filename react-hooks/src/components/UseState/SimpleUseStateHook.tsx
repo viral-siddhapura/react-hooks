e7829bdf87ce89664fb116f1516c9f7ea4f6492d
HVMS <viralsid2330@gmail.com>
2024-05-24 16:17:47 -0300

    chore: Add useState hook to SimpleUseStateHook component

--- react-hooks/src/components/UseState/SimpleUseStateHook.tsx
+++ react-hooks/src/components/UseState/SimpleUseStateHook.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SimpleUseStateHook = () => {
-    
+
+    const [value, setValue] = useState(0);
+
     return (
         <div>
-            <h1>Simple Use State hook</h1>
+            <h1>Simple UseState hook</h1>
+
+            <hr />
+
+            <button onClick={() => setValue(value + 1)}>Increment</button>
+            <button onClick={() => setValue(value - 1)}>Decrement</button>
+            <button onClick={() => setValue(0)}>Reset</button>
+
+            <hr />
+
+            <h2>Value is : {value}</h2>
         </div>
-    )
-}
+    );
+};
 
 export default SimpleUseStateHook;
